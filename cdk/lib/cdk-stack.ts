4d3885e35bff7f04b27f7629b82fbcc04ce4be41
Ridgley, Randy <22918922+randyridgley@users.noreply.github.com>
2020-10-07 15:47:35 -0400

    add cdk pre demo scripts

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,111 @@
+import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import iam = require('@aws-cdk/aws-iam');
+import glue = require('@aws-cdk/aws-glue');
+import lf = require('@aws-cdk/aws-lakeformation');
+import sagemaker = require('@aws-cdk/aws-sagemaker');
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const dataLakeRole = new iam.Role(this, 'GlueStudioPrepRole', {
+      roleName: 'GlueStudioDataLakeServiceLinkedRole',
+      assumedBy:  new iam.CompositePrincipal(
+        new iam.ServicePrincipal('glue.amazonaws.com'),
+        new iam.ServicePrincipal('lakeformation.amazonaws.com'),
+        new iam.ServicePrincipal('sagemaker.amazonaws.com'),
+      ),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AWSLakeFormationDataAdmin'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSageMakerFullAccess')
+      ]
+    });
+
+    dataLakeRole.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        'lakeformation:GetDataAccess'
+      ],
+      resources: ['*']
+    }));
+
+    dataLakeRole.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        "cloudformation:DescribeStacks"
+      ],
+      resources: ['*']
+    }));
+
+    const dataLakeBucket = new s3.Bucket(this, "DataLakeBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    dataLakeBucket.grantReadWrite(dataLakeRole);
+
+    // LakeFormation Resource registration and permissions
+    const dlResource = new lf.CfnResource(this, 'dataLakeBucketLakeFormationResource', {
+      resourceArn: dataLakeBucket.bucketArn,
+      roleArn: dataLakeRole.roleArn,
+      useServiceLinkedRole: false
+    });
+
+    const dlPermission = new lf.CfnPermissions(this, 'DataLakeLocationPermission', {
+      dataLakePrincipal: {
+        dataLakePrincipalIdentifier: dataLakeRole.roleArn,
+      },
+      resource: {
+        dataLocationResource: {
+          s3Resource: dataLakeBucket.bucketArn
+        }
+      },
+      permissions: [
+        'DATA_LOCATION_ACCESS'
+      ]
+    });
+    dlPermission.node.addDependency(dlResource)
+
+    const db = new glue.Database(this, 'TaxiGlueStudioDatabase', {
+      databaseName: 'taxi_demo'
+    });
+
+    const datalakeCrawler = new glue.CfnCrawler(this, 'DataLakeCrawler', {
+      role: dataLakeRole.roleArn,
+      databaseName: db.databaseName,
+      targets: {
+        s3Targets: [
+          {
+            path: 's3://' + dataLakeBucket.bucketName + '/datalake/'
+          }
+        ]
+      }
+    });
+
+    const notebook = new sagemaker.CfnNotebookInstance(this, 'GlueStudioPrepNotebook', {
+      notebookInstanceName: "GlueStudioPrepNotebook",
+      defaultCodeRepository: 'https://github.com/randyridgley/glue-studio-taxi-demo',
+      roleArn: dataLakeRole.roleArn,
+      instanceType: 'ml.t2.medium'
+    });
+
+    new cdk.CfnOutput(this, 'TaxiDatabase', {
+      value: db.databaseName,
+    });
+
+    new cdk.CfnOutput(this, 'TaxiDataCrawler', {
+      value: db.databaseName,
+    });
+
+    new cdk.CfnOutput(this, 'DataLakeBucketName', {
+      value: dataLakeBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, 'DataLakeRoleArn', {
+      value: dataLakeRole.roleArn,
+    });
+
+    new cdk.CfnOutput(this, 'SageMakerNotebook', {
+      value: notebook.ref,
+    });
+  }
+}
